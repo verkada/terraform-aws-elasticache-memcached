4b0c1809fb14ebb7d96ef414036ed65a88e4fe58
vikasrao23 <100244218+vikasrao23@users.noreply.github.com>
2022-03-31 10:28:43 -0700

    3.1.0 supports arm64.

--- versions.tf
+++ versions.tf
@@ -6,9 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+
     null = {
       source  = "hashicorp/null"
-      version = "~> 2.0"
+      version = ">= 3.1.0, < 4"
     }
   }
 }
